c3f5c2317cf163426d1b30256dde02f9b10d2e45
Renerd-max <maritine600@gmail.com>
2022-10-30 19:22:47 +0100

    final commit

--- server.js
+++ server.js
@@ -1,7 +1,43 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const {logger} = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const cors = require('cors');
 const PORT = process.env.PORT || 3500;
+
+//we use app.use to use middleware. middleware are functions between the request and response method.
+//the below built-in middle ware is used to handle form data. 
+
+app.use(express.urlencoded({extended: false}));
+
+//the below built-in middleware is used to handle JSON
+
+app.use(express.json());
+
+//the below built-in middleware is used to handle static files. e.g is images. Our static files needs to be in the public folder.
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+//the below custom middleware logs activities to the log folder.
+app.use(logger);
+
+//Cross Origin Resource Sharing (Cors) allows your files accessible from third parties.
+//first, to prevent access from unauthorized domains, you need to create a white list: Your website, your server, and your localhost. You should leave only your website url(s) after development.
+const whitelist = ["https://www.yourdomainname.com", "https://yourdomainname.com", "http://yourdomainname.com", "http://127.0.0.1:5500", "http://localhost:3500"];
+const corsOptions = {
+    origin: (origin, callback) => {
+        //remove "|| !origin" after development.
+
+        if (whitelist.indexOf(origin) !== -1 || !origin ) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 app.get('^/$|/index(.html)?', (req, res) => {
     //res.sendFile('./views/index.html', { root: __dirname });
@@ -43,8 +79,20 @@ const three = (req, res) => {
 
 app.get('/chain(.html)?', [one, two, three]);
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+//wec can use app.all to specify all extension types because it accepts regular expression.
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({error: 'Page not found (404)'});
+    } else {
+        res.type('txt').send("404. Page not found");
+    }
 })
+
+//custom middleware to handle error, instead of the default.
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
